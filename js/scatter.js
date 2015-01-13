9dd6cb27a12defd5bbba23bb08629389f3f423e6
Dan MacLean <dan.maclean@tsl.ac.uk>
2015-01-13 14:34:48 +0000

    spinner add

--- js/scatter.js
+++ js/scatter.js
@@ -407,6 +407,7 @@ Dropzone.options.mySecondAwesomeDropzone = {
       },
       success: function(file, response_json){
         //console.log(response_json);
+		spinner.stop();
         $("#output").css("display", "block");
         //d3.json(response_json, draw);
 		var data = JSON.parse( response_json );
@@ -417,21 +418,21 @@ Dropzone.options.mySecondAwesomeDropzone = {
 
 function spinner_opts(){
 	return {
-  lines: 17, // The number of lines to draw
-  length: 40, // The length of each line
-  width: 10, // The line thickness
-  radius: 56, // The radius of the inner circle
-  corners: 1, // Corner roundness (0..1)
-  rotate: 42, // The rotation offset
-  direction: 1, // 1: clockwise, -1: counterclockwise
-  color: '#000', // #rgb or #rrggbb or array of colors
-  speed: 1.6, // Rounds per second
-  trail: 100, // Afterglow percentage
-  shadow: false, // Whether to render a shadow
-  hwaccel: false, // Whether to use hardware acceleration
-  className: 'spinner', // The CSS class to assign to the spinner
-  zIndex: 2e9, // The z-index (defaults to 2000000000)
-  top: '50%', // Top position relative to parent
-  left: '50%' // Left position relative to parent
-};
+		lines: 13, // The number of lines to draw
+		length: 40, // The length of each line
+		width: 23, // The line thickness
+		radius: 56, // The radius of the inner circle
+		corners: 1, // Corner roundness (0..1)
+		rotate: 8, // The rotation offset
+		direction: 1, // 1: clockwise, -1: counterclockwise
+		color: '#000', // #rgb or #rrggbb or array of colors
+		speed: 0.5, // Rounds per second
+		trail: 22, // Afterglow percentage
+		shadow: true, // Whether to render a shadow
+		hwaccel: false, // Whether to use hardware acceleration
+		className: 'spinner', // The CSS class to assign to the spinner
+		zIndex: 2e9, // The z-index (defaults to 2000000000)
+		top: '50%', // Top position relative to parent
+		left: '50%' // Left position relative to parent
+	};
 }
