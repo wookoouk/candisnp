82c4dbf2b91e0e557e86abaf572b25be138de1ca
Dan MacLean <maclean.daniel@gmail.com>
2015-01-31 09:54:50 +0000

    remove dependency on bio-gngm. Typos.

--- pileup_to_snps.rb
+++ pileup_to_snps.rb
@@ -8,11 +8,31 @@
 #
 require 'rubygems'
 require 'bio-samtools'
-require 'bio-gngm'
+
+class Bio::DB::FastaLengthDB
+  require 'bio'
+  def initialize(args)
+    @file = args[:file]
+    @seqs = {}
+    file = Bio::FastaFormat.open(@file)
+    file.each do |entry|
+      @seqs[entry.entry_id] = entry.length
+    end
+    
+    def each
+      @seqs.keys.sort.each do |k|
+        yield k, @seqs[k]
+      end
+    end
+    
+  end
+end
+
+
 
 sequences = Bio::DB::FastaLengthDB.new(:file => ARGV[0])
 
-bam - Bio::DB::Sam.new(:bam=>ARGV[1], :fasta=>ARGV[0])
+bam = Bio::DB::Sam.new(:bam=>ARGV[1], :fasta=>ARGV[0])
 bam.open
 
 puts ["Chr", "Pos","Ref","Alt","Allele_Freq"].join(",")
@@ -27,7 +47,7 @@ sequences.each do |id,length|
     if pileup.is_snp?(:ignore_reference_n => true, :min_depth => 6, :min_non_ref_count => 3) and pileup.consensus != pileup.ref_base
       mut = "FALSE"
       mut = "TRUE" if (pileup.ref_base == 'G' and pileup.consensus == 'A') or (pileup.ref_base == 'C' and pileup.consensus == 'T')
-      puts [pileup.ref_name, pileup.pos, pileup.ref_base, pileup.consensu, pileup.non_ref_count/pileup.coverage, mut].join(",")
+      puts [pileup.ref_name, pileup.pos, pileup.ref_base, pileup.consensus, pileup.non_ref_count/pileup.coverage, mut].join(",")
     end
   end
 end
