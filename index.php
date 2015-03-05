630c9de6284f09a072d5ab40cbd8d8d315f1ecba
Martin Page <wookoouk@gmail.com>
2015-03-05 13:02:31 +0000

    test

--- index.php
+++ index.php
@@ -72,8 +72,8 @@
             $.getJSON('http://candisnp.tsl.ac.uk:8080/' + sessionID, function (json) {
                 var outputDiv = $("#output");
                 outputDiv.css("display", "block");
-//                pageData = json;
-                pageData = JSON.parse(JSON.parse(json));
+                pageData = json;
+//                pageData = JSON.parse(json);
                 draw(pageData, species);
                 $("html, body").delay(100).animate({scrollTop: outputDiv.offset().top}, 2000);
             });
